7275230f38159ccb842843e394c269bc4698a19e
AX2 <186868343+ax2-dev@users.noreply.github.com>
2025-11-03 14:04:42 -0500

    Added new routes and linking on main page

--- server.js
+++ server.js
@@ -8,4 +8,24 @@ app.get('/', (req,res) =>{
     res.render('index',{ip:req.ip,headers:req.headers['user-agent']});
 });
 
+app.get('/users', (req,res) =>{
+    res.send('List of users')
+});
+
+app.get('/signup', (req,res) =>{
+    res.send('Sign up form');
+});
+
+app.get('/login',(req,res) =>{
+    res.send('Login page');
+});
+
+app.post('/api/signup',(req,res) =>{
+    res.send('Recieved sign up data');
+});
+
+app.post('/api/login',(req,res) =>{
+    res.send('Recieved login data');
+});
+
 app.listen(3030);
