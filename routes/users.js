364ee2a312c5b261235fb0eece71bdd939e5fc8a
AX2 <186868343+ax2-dev@users.noreply.github.com>
2025-11-12 14:18:48 -0500

    Partially done with users js (will just return a string at the new routes)

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res) => {
+router.route('/').get((req,res) => {
     res.send('User list');
+}).post((req,res) =>{
+    res.send('POST req got');
 });
 
 router.get('/new',(req,res) =>{
@@ -14,8 +16,10 @@ router.route('/:id').get((req,res) =>{
 }).delete((req,res) =>{
     res.send(`Deleting User Data: ${req.param.id}`);
 }).put((req,res) =>{
-    res.send(`Updating User Data: ${req.param.id}`)
+    res.send(`Updating User Data: ${req.param.id}`);
 });
+
+const users = [{id:1,uname:'Aidan',pass:"123"},{id:2,uname:'P2',pass:"456"}];
 
 router.param('id', (req,res,next,id) =>{
     console.log(`Accessing user #${id}`);
