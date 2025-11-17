71824540791c8aa918e48528b412984cd047b625
AX2 <186868343+ax2-dev@users.noreply.github.com>
2025-11-17 13:45:20 -0500

    Added the /new route that loads the new.ejs

--- server.js
+++ server.js
@@ -7,12 +7,18 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 app.set('view engine','ejs');
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}))
 app.use('/api',apiRouter);
 app.use('/users',usersRouter);
 
 app.get('/', (req,res) =>{
     console.log('Here');
     res.render('index',{ip:req.ip,headers:req.headers['user-agent']});
+});
+
+app.get('/new', (req,res) =>{
+    res.render('users/new',{firstName:"Please enter your name"});
 });
 
 app.get('/users', (req,res) =>{
