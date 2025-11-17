6d16ce76ec2a0532d830c0bba411cc8a9e816bc5
AX2 <186868343+ax2-dev@users.noreply.github.com>
2025-11-17 14:13:25 -0500

    Added a user list route and ejs file viewable via a get request to /users

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 router.route('/').get((req,res) => {
-    res.send('User list');
+    res.render('users/list',{users:users});
 }).post((req,res) =>{
-    res.send('POST req got');
+    const fName = req.body.firstName;
+    const isValid = fName !=="";
+    if(isValid){
+        users.push({name:fName});
+        console.log(`Added user: ${fName}`);
+        console.log(`All users: ${users}`);
+        res.send("User Created");
+    }else{
+        console.log("Error adding user");
+        res.render('users/new',{firstName:fName});
+    }
+    
 });
 
 router.get('/new',(req,res) =>{
@@ -19,7 +30,9 @@ router.route('/:id').get((req,res) =>{
     res.send(`Updating User Data: ${req.param.id}`);
 });
 
-const users = [{id:1,uname:'Aidan',pass:"123"},{id:2,uname:'P2',pass:"456"}];
+// OLD STRUCTURE const users = [{id:1,uname:'Aidan',pass:"123"},{id:2,uname:'P2',pass:"456"}];
+
+const users = [{name:"Aidan"},{name:"Not Aidan"}]
 
 router.param('id', (req,res,next,id) =>{
     console.log(`Accessing user #${id}`);
